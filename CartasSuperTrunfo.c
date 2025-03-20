fcf804edcfb0f1a8476b439cfaeb420e13094b4e
Erick3lucyds <erickdesing13@gmail.com>
2025-03-20 19:07:02 +0000

    printf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 int main() {
+    
     char estado;
     char codigo[3];
     char cidade[20];
@@ -9,16 +10,28 @@ int main() {
     int populacao;
 
     printf("Digite uma letra de 'a' a 'h' representando o estado: \n");
-    scanf(" %c", &estado);
+    scanf("%c", &estado);
 
     printf("Digite a letra do estado seguida de um número de 01 a 04 \n");
-    scanf("%2s", codigo);
+    scanf("%s", &codigo);
 
     printf("Cidade: \n");
-    scanf(" %19s", cidade);
+    scanf(" %s", &cidade);
 
-    printf("Área em quilometros quadrados da cidade:");
+    printf("Área em quilometros quadrados da cidade: \n");
     scanf("%f", &area);
+
+    printf("Produto Interno Bruto da cidade: \n");
+    scanf("%f", &pib);
+
+    printf("Número de Pontos Turísticos: \n");
+    scanf(" %d", &turistico);
+    
+    printf("População: \n");
+    scanf(" %d", &populacao);
+
+    printf("Estado: %c - Codigo da carta: - %s \n", estado, codigo);
+    printf("Cidade: %s - Área: %f", cidade, area);
 
     return 0;
 }
